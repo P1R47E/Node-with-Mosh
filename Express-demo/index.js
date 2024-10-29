2cc2918442614ac189ff6f0f8cf84c9e92fc1bca
P1R47E <kingzaljaguar@gmail.com>
2024-10-29 09:49:12 +0300

    input validation

--- Express-demo/index.js
+++ Express-demo/index.js
@@ -1,3 +1,5 @@
+const Joi = require("joi");
+
 const express = require("express");
 const app = express()
 
@@ -12,24 +14,26 @@ app.get('/api/courses', (req,res) =>{
     res.send(courses)
 })
 
-// app.get('/api/courses',(req,res) =>{
-//     res.send([1,2,3])
-// })
-
 app.get ('/api/courses/:id',(req,res)  => {
    const course = courses.find(c => c.id === parseInt (req.params.id))
-   if(!course) res.status(404).send("the course of the Id hasn't been found")
+   if(!course) return res.status(404).send("the course of the Id hasn't been found")
    res.send(course);
 })
 
 
 app.post('/api/courses', (req,res) => {
+    const schema =Joi.object({
+        name:Joi.string().min(3).required()
+    });
+    const {error} = schema.validate(req.body)
+    if (error){
+         return res.status(400).send(error.details[0].message)}
     const course = {
         id: courses.length + 1,
         name: req.body.name,
     }
     courses.push(course)
-    res.send(course)
+    res.status(201).send(course)
 
 })
 
